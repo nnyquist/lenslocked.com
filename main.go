36a391c84609e6e92991a3a4e8bb6b9f6f2e1367
Nathan Nyquist <nnyquist@thinkbrg.com>
2021-02-01 16:27:00 -0500

    Created a signup page

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 var (
 	homeView    *views.View
 	contactView *views.View
+	signupView  *views.View
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -24,14 +25,21 @@ func contact(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func signup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(signupView.Render(w, nil))
+}
+
 // remember, this is the first thing that gets run when we start up our program
 func main() {
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	signupView = views.NewView("bootstrap", "views/signup.gohtml")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/signup", signup)
 	http.ListenAndServe(":3000", r)
 }
 
