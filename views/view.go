b0c6a021865e49db2d3d054147f0e01249179aca
Nathan Nyquist <nnyquist@thinkbrg.com>
2021-02-01 15:04:41 -0500

    Cleaned up our view.go code a bit

--- views/view.go
+++ views/view.go
@@ -1,14 +1,18 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"path/filepath"
+)
 
+var (
+	layoutDir   string = "views/layouts/"
+	templateExt string = ".gohtml"
+)
+
+// NewView ...
 func NewView(layout string, files ...string) *View {
-	files = append(files,
-		"views/layouts/bootstrap.gohtml",
-		"views/layouts/navbar.gohtml",
-		"views/layouts/footer.gohtml",
-	) // hardcoded for now, but will be updated later on to be dynamic
-
+	files = append(files, layoutFiles()...)
 	t, err := template.ParseFiles(files...) // ... unpack the slice into strings
 	if err != nil {
 		// our view should only be used when we are initializing the entire application
@@ -21,7 +25,18 @@ func NewView(layout string, files ...string) *View {
 	}
 }
 
+// View ...
 type View struct {
 	Template *template.Template
 	Layout   string
+}
+
+// layoutFiles returns a slice of strings representing
+// the layout files used in our application
+func layoutFiles() []string {
+	files, err := filepath.Glob(layoutDir + "*" + templateExt)
+	if err != nil {
+		panic(err)
+	}
+	return files
 }
