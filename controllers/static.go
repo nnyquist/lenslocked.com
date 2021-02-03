838b9f98eacde654a8846b851cefd0357db1cb8b
Nathan Nyquist <nnyquist@thinkbrg.com>
2021-02-02 21:00:13 -0500

    Created a controller for our static pages and updated the View type to implement the http.Handler interface

---
+++ controllers/static.go
@@ -0,0 +1,17 @@
+package controllers
+
+import "lenslocked.com/views"
+
+// NewStatic instantiates the Static type
+func NewStatic() *Static {
+	return &Static{
+		Home:    views.NewView("bootstrap", "views/static/home.gohtml"),
+		Contact: views.NewView("bootstrap", "views/static/contact.gohtml"),
+	}
+}
+
+// Static represents the static pages
+type Static struct {
+	Home    *views.View
+	Contact *views.View
+}
